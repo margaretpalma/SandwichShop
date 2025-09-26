0f95303e6f3042d056af99c50c5ebb49d58e5632
margaretpalma <mpalma1496@gmail.com>
2025-09-26 15:17:17 -0400

    initial commit

---
+++ src/main/java/com/pluralsight/SandwichShop.java
@@ -0,0 +1,17 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class SandwichShop {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in)
+
+        System.out.println("Which size sandwich would you like?");
+        System.out.println("Regular: $5.45");
+        System.out.println("Large: $8.95");
+
+
+    }
+
+}
